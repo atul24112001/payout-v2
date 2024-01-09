277f20e4bdb9af848b8de30550b2ee34e88caee9
atul24112001 <atulmorchhlay204@gmail.com>
2024-01-09 18:35:35 +0530

    Added wallet in sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,6 +2,7 @@
 import { NavLink } from "react-router-dom";
 import Icon from "../../assets/icons/ChevronDown";
 import { routes } from "../../routes";
+import WalletIcon from "../../assets/icons/Wallet";
 
 type Props = {
   setActiveTab: (title: string) => void;
@@ -16,8 +17,8 @@ export default function Sidebar({
 }: Props) {
   return (
     <div
-      style={{ display: showSidebar ? "block" : "none" }}
-      className="fixed py-4 px-3 md:static  w-screen top-0 bottom-0 sm:w-1/2 md:w-1/3 lg:w-1/5 bg-primary-background"
+      style={{ display: showSidebar ? "flex" : "none" }}
+      className="fixed py-4 px-3 md:static  w-screen top-0 bottom-0 sm:w-1/2 md:w-1/3 lg:w-1/5 bg-primary-background flex-col"
     >
       <div className="flex gap-3 items-center px-4">
         <img src="/logo.png" className="w-[39px] h-[39px] rounded-sm" />
@@ -39,7 +40,7 @@ export default function Sidebar({
           height={20}
         />
       </div>
-      <div className="mt-8">
+      <div className="mt-8 flex-1">
         {routes.map((link) => {
           const { title, IconActive, IconInactive, link: path } = link;
           return (
@@ -67,6 +68,17 @@ export default function Sidebar({
             </NavLink>
           );
         })}
+      </div>
+      <div className="bg-[#353C53] px-3 py-[6px] rounded text-white flex gap-3 items-center">
+        <div className="p-[6px] bg-[#ffffff10] rounded">
+          <WalletIcon />
+        </div>
+        <div className="">
+          <div className="text-[13px] text-white font-normal">
+            Available credits
+          </div>
+          <div className="text-[16px] text-white font-medium">222.10</div>
+        </div>
       </div>
     </div>
   );
